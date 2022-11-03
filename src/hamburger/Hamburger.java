b9142182af4ea01cbc821fb2f7258661f517b6e6
Nappy97 <wjddmlgud829@gmail.com>
2022-11-03 22:16:19 +0900

    1103

---
+++ src/hamburger/Hamburger.java
@@ -0,0 +1,26 @@
+package hamburger;
+
+import java.util.Arrays;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/133502
+public class Hamburger {
+
+
+    class Solution {
+        public int solution(int[] ingredient) {
+            int answer = 0;
+
+            StringBuilder build = new StringBuilder();
+
+            for(int i=0; i<ingredient.length; i++) {
+                build.append(ingredient[i]);
+                if(build.length()>3 && build.subSequence(build.length()-4, build.length()).equals("1231")) {
+                    answer++;
+                    build.delete(build.length()-4, build.length());
+                }
+            }
+
+            return answer;
+        }
+    }
+}
